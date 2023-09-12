57c0c5d4f6fa1822237becd259f7da9f8c527af7
Axel Franco <afranco@entaconsulting.com>
2023-09-12 18:28:31 -0300

    #47615 Setting

--- sdk/src/authentication/msalConfig.ts
+++ sdk/src/authentication/msalConfig.ts
@@ -1,7 +1,7 @@
 export const msalConfig = {
   cache: {
     cacheLocation: "localStorage", // This configures where your cache will be stored
-    storeAuthStateInCookie: true, // Set this to "true" if you are having issues on IE11 or Edge
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
   },
   // system: {
   //   loggerOptions: {
